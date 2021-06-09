5c614aa71800becd08e12a6bb37e2d50c2ad6190
Hossein Mohammadi <hussein.muhammadii@gmail.com>
2021-06-09 16:02:35 +0430

    Spring Cloud Eureka

---
+++ spring-cloud-eureka-server/src/test/java/com/javastudio/springcloud/eureka/server/SpringContextTest.java
@@ -0,0 +1,16 @@
+package com.javastudio.springcloud.eureka.server;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@SpringBootTest
+@RunWith(SpringRunner.class)
+public class SpringContextTest {
+
+    @Test
+    public void whenSpringContextIsBootstrapped_thenNoExceptions() {
+    }
+
+}
